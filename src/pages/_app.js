28ab6089f900c160fd76edcf89d381f6e2220f52
Riaz Karolia <riazkarolia@gmail.com>
2021-11-29 17:21:24 +0200

    Tracking Code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,23 @@
-import Theme from '../styles/theme';
+import Theme from "../styles/theme";
+
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    window.gtag("config", "[Tracking ID]", {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
     <>
       <Theme>
@@ -9,4 +26,3 @@ export default function App({ Component, pageProps }) {
     </>
   );
 }
- 
