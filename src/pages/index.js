33e736e9a85ae33d9261ffc099a6cd38bf2615b9
Riaz Karolia <riazkarolia@gmail.com>
2021-11-29 16:15:26 +0200

    tracking code

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,23 @@ import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation"
 import Hero from "../components/Hero/Hero";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
+import Head from "next/head";
 
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        {/*Global site tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-213806033-1"
+        />
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'UA-213806033-1');
+        </script>
+      </Head>
       <Section grid>
         <Hero />
         <BgAnimation />
